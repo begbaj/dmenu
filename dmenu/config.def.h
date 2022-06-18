4f034520e0d3a58795ede22a41bc6d088c529d06
Began <beganbajrami@gmail.com>
2022-06-18 11:10:15 +0200

    pywal

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -3,20 +3,25 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
-static const unsigned int alpha = 0xf0;
+static const unsigned int alpha = 0x00;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+/*
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
+	/*     fg         bg       
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
+
+*/
+#include "/home/began/.cache/wal/colors-wal-dmenu.h"
 
 static const unsigned int alphas[SchemeLast][2] = {
 	[SchemeNorm] = { OPAQUE, alpha },
@@ -34,4 +39,4 @@ static unsigned int lines      = 0;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 5;
+static const unsigned int border_width = 3;
