01bcac111d25f694f0163daa3a0f577e5a6456bc
Began <beganbajrami@gmail.com>
2022-10-23 20:58:41 +0200

    refacotr

--- src/config.h
+++ src/config.h
@@ -1,27 +1,25 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
-static const unsigned int alpha = 0xAA;
+static int min_width = 1200;                    /* minimum width when centered */
+static const unsigned int alpha = 0xFE;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
-/*
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       
+	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSel] = { "#eeeeee", "#005357" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 
-*/
-#include "/home/began/.cache/wal/colors-wal-dmenu.h"
+// #include "/home/began/.cache/wal/colors-wal-dmenu.h"
 
 static const unsigned int alphas[SchemeLast][2] = {
 	[SchemeNorm] = { OPAQUE, alpha },
@@ -30,7 +28,7 @@ static const unsigned int alphas[SchemeLast][2] = {
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 10;
 
 /*
  * Characters not considered part of a word while deleting words
@@ -39,4 +37,4 @@ static unsigned int lines      = 0;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 3;
+static const unsigned int border_width = 0;
